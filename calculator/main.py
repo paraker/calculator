ed30397218c2eb377a5d2241949a7f8e96678ec4
Par Akerstrom <par.w.akerstrom@gmail.com>
2019-06-15 15:21:04 +1000

    add test file and move main

---
+++ calculator/main.py
@@ -0,0 +1,9 @@
+"""
+Calculator library for basic math operations
+"""
+
+def add(first_term: int, second_term: int) -> int:
+    return first_term + second_term
+
+def subtract(first_term: int, second_term: int) -> int:
+    return first_term - second_term
