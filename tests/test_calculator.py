f664d9d3c0a2ad54f0fdd5e617aea4772901fe68
Par Akerstrom <par.w.akerstrom@gmail.com>
2019-06-15 16:15:20 +1000

    added testyaml and renamed test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,9 @@
 from app import calculator
-import pytest
+
 
 class TestCalculator:
     def test_add(self):
         assert 4 == calculator.add(2, 2)
+
     def test_sub(self):
         assert 5 == calculator.subtract(10, 5)
