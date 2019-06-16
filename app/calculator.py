a106550d6318984371d2f6a829e8e99667a6b0a8
Par Akerstrom <par.w.akerstrom@gmail.com>
2019-06-16 12:12:36 +1000

    Added function for multiplication

--- app/calculator.py
+++ app/calculator.py
@@ -9,3 +9,7 @@ def add(first_term: int, second_term: int) -> int:
 
 def subtract(first_term: int, second_term: int) -> int:
     return first_term - second_term
+
+
+def multiplication(first_term: int, second_term: int) -> int:
+    return first_term * second_term
